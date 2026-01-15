315f208a917d9256254a08c68a7e25db547e673a
atrasal <rasalaaditya246@gmail.com>
2026-01-15 15:24:51 +0530

    home page done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
+
 
 dotenv.config();
 
@@ -11,6 +13,9 @@ const PORT = process.env.PORT || 5001;
 
 //middleware
 app.use(express.json()); // to parse JSON request bodies
+app.use(cors({
+  origin: "http://localhost:5173", // frontend URL
+}));
 app.use(rateLimiter);
 // app.use((req, res, next) => {
 //   console.log(`${req.method} ${req.url}`);

cb1aba62ef0c61e09e8007a3541db8b99bad6b23
atrasal <rasalaaditya246@gmail.com>
2026-01-23 00:28:51 +0530

    Ready to be deployed

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,24 +4,39 @@ import dotenv from 'dotenv';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
+import path from 'path';
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware
-app.use(express.json()); // to parse JSON request bodies
-app.use(cors({
+if(process.env.NODE_ENV !== 'production') {
+  app.use(cors({
   origin: "http://localhost:5173", // frontend URL
 }));
+}
+
+app.use(express.json()); // to parse JSON request bodies
+
 app.use(rateLimiter);
 // app.use((req, res, next) => {
 //   console.log(`${req.method} ${req.url}`);
 //   next();
 // })
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+});
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -29,4 +44,3 @@ connectDB().then(() => {
 });
 
 
-//mongodb+srv://rasalaaditya246_db_user:dUE3HJNGlKk8shF3@cluster0.abjcptu.mongodb.net/?appName=Cluster0
